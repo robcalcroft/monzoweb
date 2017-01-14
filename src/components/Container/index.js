fda887cb2a7d92df4369ff2876d96647586ced4f
Rob Calcroft <rob@calcroft.com>
2017-01-14 16:32:03 +0000

    (#68) - Moved components into functions

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import Nav from 'components/Nav';
 
-export default class Container extends React.Component {
-  render() {
-    return (
-      <div className={localStorage.monzo_access_token ? 'loggedIn' : 'loggedOut'}>
-        <Nav />
-        <div className="wrapper">
-          {this.props.children}
-        </div>
-      </div>
-    );
-  }
-}
+export default ({ children }) => (
+  <div className={localStorage.monzo_access_token ? 'loggedIn' : 'loggedOut'}>
+    <Nav />
+    <div className="wrapper">
+      {children}
+    </div>
+  </div>
+);
--- src/components/NoMatch/index.js
+++ src/components/NoMatch/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 
-export default class NoMatch extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>404</h1>
-      </div>
-    );
-  }
-}
+
+export default () => (
+  <div>
+    <h1>404</h1>
+  </div>
+);
