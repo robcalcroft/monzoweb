4f7cf91f532ec8c5a8110af7e827056aa7b3a2ea
Rob Calcroft <rob@calcroft.com>
2018-02-02 18:09:37 +0000

    fix(Login): Move text line break

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -6,7 +6,10 @@ class Login extends React.PureComponent {
     return (
       <div className="mzw-login">
         <div>
-          <div className="mzw-login__label">Click Login below to authorize <br /> Monzo Web to use your Monzo account.</div>
+          <div className="mzw-login__label">
+            Click Login below to authorize<br />
+            Monzo Web to use your Monzo account.
+          </div>
           <a href={`https://auth.getmondo.co.uk/?client_id=${process.env.MONZO_CLIENT_ID}&redirect_uri=${process.env.MONZO_REDIRECT_URI}&response_type=code`}>Login</a>
         </div>
       </div>
